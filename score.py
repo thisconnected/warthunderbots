c921d0db22370add9cc5e70734846a418535d2eb
Patryk Kaniewski <p7tryk@gmail.com>
2023-05-08 18:19:36 +0200

    initial commit

---
+++ score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from urllib.request import urlopen
+import os
+import configparser
+
+
+config = configparser.ConfigParser()
+url = "https://warthunder.com/en/community/claninfo/" + 
